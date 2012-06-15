9a403ad22418f2007387d394be01939251ab5294
rrae <rrae@localhorst.(none)>
2012-06-15 21:25:57 +0200

    modified:   fetch.py

--- fetch.py
+++ fetch.py
@@ -1,36 +1,50 @@
 # -*- coding: utf-8 -*-
 from urllib2 import urlopen
-from lxml.html import soupparser
-from lxml import etree
+from lxml import etree, html
 from os import path, system
 from xml.dom import minidom
 
 __author__ = "C.Wilhelm"
 __license__ = "AGPL v3"
 
-def write_html(url, short_name):
-	html = urlopen(url).read()
-	element = soupparser.fromstring(html)
+def write_html(url, filename):
+	root_element = etree.Element("Gesetz")
+	content = urlopen(url).read()
+	element = html.fromstring(content)
+	#for element in element.xpath("//*[@id = 'container']//*[@title != 'NaN']"):
+	#	print element.attrib
+	element = element.xpath("//*[@id = 'paddingLR12']")[0]
+	for unwanted in element.xpath("//*[name() = 'a' or name() = 'script']"):
+		unwanted.getparent().remove(unwanted)
+	### prepare HTML tables (pandoc cannot handle them)
+	#handle_html_tables(element)
 	output = etree.tostring(element, encoding="utf-8", method="xml")
 	output = minidom.parseString(output).toprettyxml()
-	f = open('tmp/'+short_name, 'w')
+	#output = element.xpath("string()")
+	f = open('tmp/'+filename, 'w')
 	f.write(output.encode("utf-8"))
 	f.close()
+	write_markdown(	url='tmp/'+filename,
+					filename='tmp/'+filename)
 
-def write_markdown(url, short_name):
-	system("pandoc -s -r html %s -o tmp/%s" % (url, short_name))
+def write_markdown(url, filename):
+	system("pandoc -s -r html %s -o %s.md" % (url, filename))
 
-uri = "http://www.gesetze-im-internet.de/"
-nopdf = "substring(@href, string-length(@href)-3)!='.pdf'" # lxml supports XPath 1.0
-html = urlopen(uri+"aktuell.html").read()
-element = soupparser.fromstring(html)
-for link in element.xpath("//a[number(text())]"):
-	html = urlopen(uri+link.attrib["href"]).read()
-	element = soupparser.fromstring(html)
-	for link in element.xpath("//*[@id = 'container']//a[%s]" % nopdf):
-		response = urlopen(uri+link.attrib["href"])
-		element = soupparser.fromstring(response.read())
-		current_url = path.dirname(response.geturl())
-		short_name = path.split(current_url)[-1]#.strip("-_123456789")
-		for link in element.xpath("//h2//a[%s]" % nopdf):
-			write_html(current_url+"/"+link.attrib["href"], short_name)
+def fetch():
+	uri = "http://www.gesetze-im-internet.de/"
+	nopdf = "substring(@href, string-length(@href)-3)!='.pdf'" # lxml supports XPath 1.0
+	content = urlopen(uri+"aktuell.html").read()
+	element = html.fromstring(content)
+	for link in element.xpath("//a[@class = 'alphabet']"):
+		content = urlopen(uri+link.attrib["href"]).read()
+		element = html.fromstring(content)
+		for link in element.xpath("//*[@id = 'container']//a[%s]" % nopdf):
+			response = urlopen(uri+link.attrib["href"])
+			element = html.fromstring(response.read())
+			current_url = path.dirname(response.geturl())
+			short_name = path.split(current_url)[-1]#.strip("-_123456789")
+			for link in element.xpath("//h2//a[%s]" % nopdf):
+				write_html(current_url+"/"+link.attrib["href"], short_name)
+
+#write_html("http://www.gesetze-im-internet.de/alg/BJNR189100994.html", "alg")
+fetch()
