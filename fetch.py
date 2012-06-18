cbfb3af6adbc8c10231d19cde1629a5ca3b41f95
rrae <rrae@localhorst.(none)>
2012-06-18 11:20:24 +0200

    modified:   fetch.py

--- fetch.py
+++ fetch.py
@@ -11,18 +11,16 @@ def write_html(url, filename):
 	root_element = etree.Element("Gesetz")
 	content = urlopen(url).read()
 	element = html.fromstring(content)
-	#for element in element.xpath("//*[@id = 'container']//*[@title != 'NaN']"):
-	#	print element.attrib
 	element = element.xpath("//*[@id = 'paddingLR12']")[0]
 	for unwanted in element.xpath("//*[name() = 'a' or name() = 'script']"):
 		unwanted.getparent().remove(unwanted)
 	### prepare HTML tables (pandoc cannot handle them)
 	#handle_html_tables(element)
 	output = etree.tostring(element, encoding="utf-8", method="xml")
-	output = minidom.parseString(output).toprettyxml()
+	#output = minidom.parseString(output).toprettyxml()
 	#output = element.xpath("string()")
 	f = open('tmp/'+filename, 'w')
-	f.write(output.encode("utf-8"))
+	f.write(output)#.encode("utf-8"))
 	f.close()
 	write_markdown(	url='tmp/'+filename,
 					filename='tmp/'+filename)
@@ -46,6 +44,7 @@ def fetch():
 			for link in element.xpath("//h2//a[%s]" % nopdf):
 				write_html(current_url+"/"+link.attrib["href"], short_name)
 
-#write_html("http://www.gesetze-im-internet.de/alg/BJNR189100994.html", "alg") # contains tables
-#write_html("http://www.gesetze-im-internet.de/bgb/BJNR001950896.html", "bgb")
-fetch()
+write_html("http://www.gesetze-im-internet.de/alg/BJNR189100994.html", "alg") # contains tables
+write_html("http://www.gesetze-im-internet.de/bgb/BJNR001950896.html", "bgb")
+write_html("http://www.gesetze-im-internet.de/bgb/BJNR001950896.html", "hgb")
+#fetch()
