770b855ab6a3daaf107f62279f5f7e7677fad241
rrae <rrae@localhorst.(none)>
2012-07-09 14:58:30 +0200

    modified:   fetch.py 	new file:   test_ajax.html

--- fetch.py
+++ fetch.py
@@ -1,32 +1,14 @@
 # -*- coding: utf-8 -*-
 from urllib2 import urlopen
 from lxml import etree, html
-from os import path, system
 from xml.dom import minidom
 
 from zipfile import ZipFile, BadZipfile
 from StringIO import StringIO
+from os import listdir, path
 
 __author__ = "C.Wilhelm"
 __license__ = "AGPL v3"
-
-def write_html(url, filename):
-	root_element = etree.Element("Gesetz")
-	content = urlopen(url).read()
-	element = html.fromstring(content)
-	element = element.xpath("//*[@id = 'paddingLR12']")[0]
-	for unwanted in element.xpath("//*[name() = 'a' or name() = 'script']"):
-		unwanted.getparent().remove(unwanted)
-	### prepare HTML tables (pandoc cannot handle them)
-	#handle_html_tables(element)
-	output = etree.tostring(element, encoding="utf-8", method="xml")
-	#output = minidom.parseString(output).toprettyxml()
-	#output = element.xpath("string()")
-	f = open('tmp/'+filename, 'w')
-	f.write(output)#.encode("utf-8"))
-	f.close()
-	write_markdown(	url='tmp/'+filename,
-					filename='tmp/'+filename)
 
 def write_xml(url, filename):
 	remotefile = urlopen(url)
@@ -36,8 +18,8 @@ def write_xml(url, filename):
 	zipfile.extractall(path="tmp/%s/" % filename)
 	zipfile.close()
 
-def write_markdown(url, filename):
-	system("pandoc --atx-headers -s -r html %s -o %s.md" % (url, filename))
+def write_markdown(filename_in, filename_out):
+	system("pandoc --atx-headers -s -r html %s -o %s.md" % (filename_in, filename_out))
 
 def fetch():
 	uri = "http://www.gesetze-im-internet.de/"
@@ -56,9 +38,49 @@ def fetch():
 			for link in element.xpath("//h2//a[%s]" % xpath_xmlonly):
 				write_xml(current_url+"/"+link.attrib["href"], filename=short_name)
 
+def index():
+	"extract metadata for all fetched xml files in ./tmp/"
+	# structure: title | filename | [keyword1,keyword2,title]
+	# example: ./tmp/alg/BJNR189100994.xml | [ALG]
+	dtd = etree.DTD("gii-norm.dtd")
+	laws = etree.Element("laws")
+	for name in listdir("./tmp/"):
+		subpath = path.join("./tmp/", name)
+		if not path.isdir(subpath):
+			continue
+		pname = name # the index.xml should contain relative pathes
+		for name in listdir(subpath):
+			if name.endswith(".xml"):
+				keywords = set()
+				titles = set()
+				filename = path.join(subpath, name)
+				dokumente = etree.parse(filename)
+				assert(dtd.validate(dokumente))
+				# <!ELEMENT dokumente (norm*)> # Regelfall: erste "Norm" enthält Metadaten für Gesamtdokument, zweite "Norm" enthält Inhaltsverzeichnis
+				firstnorm = dokumente.find("norm")
+				# <!ELEMENT metadaten (jurabk+, amtabk?, ausfertigung-datum?, fundstelle*, kurzue?, langue?, gliederungseinheit?, enbez?, titel?, standangabe*)>
+				keywords.update(firstnorm.xpath("metadaten/jurabk/text()"))
+				keywords.update(firstnorm.xpath("metadaten/amtabk/text()"))
+				titles.update(firstnorm.xpath("metadaten/langue/text()"))
+				titles.update(firstnorm.xpath("metadaten/titel/text()"))
+				keywords.update(titles)
+				assert(len(titles) == 1) # didn't occur yet, would need to be handled
+				law = etree.SubElement(laws, "law")
+				etree.SubElement(law, "title").text = titles.pop()
+				etree.SubElement(law, "filename").text = path.join(pname, name)
+				kwords = etree.SubElement(law, "keywords")
+				for keyword in keywords:
+					etree.SubElement(kwords, "keyword").text = keyword
+	xmlstr = etree.tostring(laws, pretty_print=True)
+	f = open("tmp/index.xml", "w")
+	f.write(xmlstr)
+	f.close()
+
 write_xml("http://www.gesetze-im-internet.de/alg/xml.zip", "alg") # contains tables
 write_xml("http://www.gesetze-im-internet.de/bgb/xml.zip", "bgb")
 write_xml("http://www.gesetze-im-internet.de/hgb/xml.zip", "hgb")
 write_xml("http://www.gesetze-im-internet.de/gmbhg/xml.zip", "gmbhg")
 write_xml("http://www.gesetze-im-internet.de/aktg/xml.zip", "aktg")
+write_xml("http://www.gesetze-im-internet.de/gg/xml.zip", "gg")
 #fetch()
+index()
